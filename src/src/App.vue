b111217ec957951eba3a874dbbcfa83a2315bee1
Илья Самойлов <141013914+PressureFlow@users.noreply.github.com>
2023-11-13 22:40:37 +0300

    first commit

---
+++ src/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component"/>
+    </transition>
+
+  </router-view>
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: sans-serif, 'Montseratt';
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+ul {
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  margin: 0 auto;
+  padding: 0;
+  font-size: large;
+}
+
+li {
+  list-style: none;
+  padding: 7px;
+  border: 1px solid black;
+  border-radius: 7px;
+}
+
+li a {
+  text-decoration: none;
+  color: black;
+}
+li:hover {
+  color: white;
+  background-color: rgb(203, 203, 203);
+  transition: all  0.5s ease-in-out;
+}
+
+</style>
